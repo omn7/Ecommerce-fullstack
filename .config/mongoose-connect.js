882ac6bc064f886a0a9ce138bab18385491216b1
Om Narkhede <developers.omn@gmail.com>
2025-01-28 23:45:31 +0530

    js

--- .config/mongoose-connect.js
+++ .config/mongoose-connect.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dbgr = require('debug')("developement:mongoose");
+const config = require('config');
 
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log('Connected to database');
+
+mongoose.connect(`${config.get("MONGODB_URI")}/scatch`).then(() => {
+    dbgr('Connected to database');
 }).catch((err) => {
-    console.log('Error connecting to database', err);
+    dbgr('Error connecting to database', err);
 });
 module.exports = mongoose.connection; 
